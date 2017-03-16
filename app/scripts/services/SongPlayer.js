690aba7c08b5488bcb217688d81d7d3d35adbf43
Naima <naimaoni@gmail.com>
2017-03-15 20:53:03 -0400

    assignment 7 services part2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,12 +3,22 @@
         var SongPlayer = {};
         
         /**
-        * @desc Buzz object audio file
+        * @desc Current song data that is playing or paused (number, title, duration) (private attribute)
         * @type {Object}
         */
         var currentSong = null;
+        
+        /**
+        * @desc Buzz object audio file (private attribute)
+        * @type {Object}
+        */
         var currentBuzzObject = null;
         
+        /**
+        * @function setSong (private function)
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -21,33 +31,47 @@
             });
             
             currentSong = song;
-        }
+        };
         
+        /**
+        * @function playSong (private function)
+        * @desc Play the current Buzz object audio file set song attribute to true
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        /**
+        * @function SongPlayer.play (public method)
+        * @desc Play current song if song is
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                /**
-                * @function setSong
-                * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-                * @params {Oject} song
-                */
                 setSong(song);
-                currentBuzzObject.play();
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+                    playSong();
                 }
             }
     
         };
         
+        /**
+        * @function SongPlayer.pause (public method)
+        * @desc Pause current Buzz object and set song.playing to false
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
-        }
+        };
         
         return SongPlayer;
-    }
+    };
     
     angular
         .module('blocJams')
